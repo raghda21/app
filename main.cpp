436ed8a2cdd8a7d6fdd2ebc78191b395ff11c305
~raghd21~ <~raghdasalem40@gmail.com~>
2020-02-10 20:01:55 +0200

    first commit

---
+++ main.cpp
@@ -0,0 +1,59 @@
+
+#include <stdio.h>
+#include <winsock2.h>  //socket ap's
+
+#pragma comment(lib,"ws2_32.lib")
+int main ()
+{
+    WSADATA wsa;  //only for windows
+    SOCKET s;  //socket descriptor
+   struct sockaddr_in server;    //only for windows
+   printf("\n INitialising Winsock....");
+   if (WSAStartup(MAKEWORD(2,2),&wsa)  != 0)
+   {
+       printf("faild error code :%d",WSAGetLastError());
+       return 1;
+   }
+   printf("initilazing.\n");
+
+
+//creat a socket
+   //we have to first create discriptor and give 1-socket family the 2-socket stream type 3-protocal(usually 0)
+   if((s=socket (AF_INET ,SOCK_STREAM,0))== INVALID_SOCKET)
+   {
+       printf("could not creat socket :%d",WSAGetLastError());
+   }
+   printf("Socket created successfully \n ");
+
+
+   server.sin_family=AF_INET;  //ADDRESS FAMILY IPv4
+   server.sin_port=htons(80); //putting the host port (http port 80)
+   server.sin_addr.s_addr=inet_addr("184.106.153.149");  //putting server address into descriptor
+
+ while(1){printf("done");
+     //connection to remote server using TCP/IP
+   connect(s,(struct sockaddr *)&server,sizeof(server));
+   if(connect(s,(struct sockaddr*)&server , sizeof(server)),0){
+       puts("Connect Error");
+   }
+   else{
+       puts("Connection Initialized Successfully.\n");
+   }
+  /* char message[50]="GET /HTTP/1.1\r\n\r\n";
+  // char recv_buf[2000]="";
+
+   send(s,message,strlen(message),0);
+  // print("%d",5);
+  // recv(s,recv_buf,2000,0);
+   //puts(recv_buf);*/
+
+   char  message="GET https://api.thingspeak.com/update?api_key=UC8VPHBURX6UGSOY&field1=20\r\n\r\n"
+               send(s,message,strlen(message),0);
+
+
+   closesocket (s);
+}
+
+return 0;
+
+}
---
+++ temp_client.pro
@@ -0,0 +1,18 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2020-02-06T13:31:27
+#
+#-------------------------------------------------
+
+QT       += core
+
+QT       -= gui
+
+TARGET = temp_client
+CONFIG   += console
+CONFIG   -= app_bundle
+
+TEMPLATE = app
+
+LIBS += C:\Qt\Qt5.2.0\Tools\mingw48_32\i686-w64-mingw32\lib\libwsock32.a
+SOURCES += main.cpp
